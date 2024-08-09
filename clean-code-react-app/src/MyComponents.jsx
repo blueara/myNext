2db23503950f8c533b58117b838f8d9225b9cca8
Dongyeong Kim <userset301@gmail.com>
2024-08-09 20:59:52 +0900

    Initial commit

---
+++ clean-code-react-app/src/MyComponents.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+const MyComponents = ({ name, favoriteNumber, children }) => {
+  return (
+    <div>
+      안녕하세요 제 이름은 {name} 입니다. children 값은 {children}입니다.
+      <br>제가 좋아하는 숫자는 {favoriteNumber}입니다.</br>
+    </div>
+  );
+};
+
+MyComponents.defaultProps = {
+  name: "기본이름",
+};
+
+MyComponents.propTypes = {
+  name: PropTypes.string,
+  favoriteNumber: PropTypes.number.isRequired,
+};
+
+export default MyComponents;
