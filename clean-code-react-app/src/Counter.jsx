8789ef43fa0cbb568cebf3e0828848d98439c07c
Dongyeong Kim <userset301@gmail.com>
2024-08-09 21:09:52 +0900

    useReducer 공부
    useReducer

--- clean-code-react-app/src/Counter.jsx
+++ clean-code-react-app/src/Counter.jsx
@@ -1,42 +1,27 @@
-import { Component } from "react";
+import { useReducer } from "react";
 
-class Counter extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   //state 의 초깃값 설정하기
-  //   this.state = {
-  //     number: 0,
-  //     fixedNumber: 0,
-  //   };
-  // }
-  state = {
-    number: 0,
-    fixedNumber: 0,
-  };
-  render() {
-    const { number, fixedNumber } = this.state; //state를 조회할 때는 this.state로 조회합니다.
-    return (
-      <div>
-        <h1>{number}</h1>
-        <h2>바뀌지 않는 값: {fixedNumber}</h2>
-        <button
-          onClick={() => {
-            this.state(
-              {
-                number: number + 1,
-              },
-              () => {
-                console.log("방금 setState가 호출되었습니다.");
-                console.log(this.state);
-              }
-            );
-          }}
-        >
-          +1
-        </button>
-      </div>
-    );
+function reducer(state, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return { value: state.value + 1 };
+    case "DECREMENT":
+      return { value: state.value - 1 };
+    default:
+      return state;
   }
 }
+const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, { value: 0 });
+
+  return (
+    <div>
+      <p>
+        현재 카운터 값은 <b>{state.value}</b>입니다.
+      </p>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>+1</button>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>-1</button>
+    </div>
+  );
+};
 
 export default Counter;
