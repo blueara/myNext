100d9c2ceeabb77f992fcb57aefb204228bf8b41
Dongyeong Kim <userset301@gmail.com>
2024-08-10 20:03:34 +0900

    average.jsx useCallback

--- clean-code-react-app/src/Average.jsx
+++ clean-code-react-app/src/Average.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useCallback } from "react";
 
 const getAverage = (numbers) => {
   console.log("평균값 계산 중");
@@ -6,19 +6,23 @@ const getAverage = (numbers) => {
   const sum = numbers.reduce((a, b) => a + b);
   return sum / numbers.length;
 };
+
 const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState("");
 
-  const onChange = (e) => {
+  const onChange = useCallback((e) => {
     setNumber(e.target.value);
-  };
-  const onInsert = (e) => {
+  }, []);
+
+  const onInsert = useCallback(() => {
     const nextList = list.concat(parseInt(number));
     setList(nextList);
     setNumber("");
-  };
+  }, [number, list]);
+
   const avg = useMemo(() => getAverage(list), [list]);
+
   return (
     <div>
       <input value={number} onChange={onChange} />
