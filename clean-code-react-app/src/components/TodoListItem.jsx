3886b52ed0e5a137387d97060a3eefad0c4c1057
Dongyeong Kim <userset301@gmail.com>
2024-08-15 21:19:17 +0900

    Todo app

--- clean-code-react-app/src/components/TodoListItem.jsx
+++ clean-code-react-app/src/components/TodoListItem.jsx
@@ -3,17 +3,20 @@ import {
   MdCheckBox,
   MdRemoveCircleOutline,
 } from "react-icons/md";
+import cn from "classnames";
 import "./TodoListItem.scss";
 
-const TodoListItem = () => {
+const TodoListItem = ({ todo }) => {
+  const { text, checked } = todo;
   return (
     <div className="TodoListItem">
-      <div className="checkbox">
-        <MdremoveOutlineBlank />
-        <div className="text">할 일</div>
+      <div className={cn("checkbox", { checked })}>
+        {checked ? <MdCheckBox /> : <MdremoveOutlineBlank />}
+
+        <div className="text">{text}</div>
       </div>
       <div className="remove">
-        <MdremoveCircleOutline />
+        <MdRemoveCircleOutline />
       </div>
     </div>
   );
